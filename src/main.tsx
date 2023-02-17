40d85269b624df200d13d96e0792c945f5d6d019
hongshengjie <hongshengjie@bilibili.com>
2023-02-17 15:02:05 +0800

    add fsWatcher

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
+import { watch, watchImmediate } from "tauri-plugin-fs-watch-api";
+
+// can also watch an array of paths
+const stopWatching = await watchImmediate(
+  "/Users/hongshengjie/web/hostsman/README.md",
+  {  },
+  (event) => {
+    console.log(event)
+  }
+);
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
